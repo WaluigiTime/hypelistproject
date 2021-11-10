b4f76b25c8e356af944cbf814741285c1e065900
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-11-10 18:38:05 -0500

    made a names list? still broken though. pain, suffering even

--- src/App.js
+++ src/App.js
@@ -2,25 +2,61 @@ import "./styles.css";
 //Write array below
 export default function App() {
   return (
-    <div className="App">
+    <div className="First Names">
       <table>
         <th>First Name</th>
         <tr>
-          <td>Name</td>
+          <td>{hypeNames[0]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{hypeNames[1]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{hypeNames[2]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{hypeNames[3]}</td>
         </tr>
         <tr>
-          <td>Name</td>
+          <td>{hypeNames[4]}</td>
         </tr>
       </table>
     </div>
+
+    /*
+<div className="Places">
+
+[ Confused as to why this code
+1. makes it to where I cannot use the arrays
+2. breaks everything ]
+
+<table>
+  <th>First Name</th>
+  <tr>
+    <td>{hypePlaces[0]}</td>
+  </tr>
+  <tr>
+    <td>{hypePlaces[1]}</td>
+  </tr>
+  <tr>
+    <td>{hypePlaces[2]}</td>
+  </tr>
+  <tr>
+    <td>{hypePlaces[3]}</td>
+  </tr>
+  <tr>
+    <td>{hypePlaces[4]}</td>
+  </tr>
+</table>
+</div>
+*/
   );
 }
+
+let hypeNames = ["Melody", "Maxwell", "Jean", "Judy", "Reginea"];
+
+let hypePlaces = [];
+
+let hypeColors = [];
+
+let hype;
