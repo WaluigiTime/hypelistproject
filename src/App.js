42cbb03a34dbb81e67b3c8f03fe8581604c24670
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-11-17 18:25:58 -0500

    fixed list issues now i just have to finish them all ;-;

--- src/App.js
+++ src/App.js
@@ -21,41 +21,49 @@ export default function App() {
           <td>{hypeNames[4]}</td>
         </tr>
       </table>
+      <table>
+        <th>Places</th>
+        <tr>
+          <td>{hypePlaces[0]}</td>
+        </tr>
+        <tr>
+          <td>{hypePlaces[1]}</td>
+        </tr>
+        <tr>
+          <td>{hypePlaces[2]}</td>
+        </tr>
+        <tr>
+          <td>{hypePlaces[3]}</td>
+        </tr>
+        <tr>
+          <td>{hypePlaces[4]}</td>
+        </tr>
+      </table>
+      <table>
+        <th>Places</th>
+        <tr>
+          <td>{hypeColors[0]}</td>
+        </tr>
+        <tr>
+          <td>{hypeColors[1]}</td>
+        </tr>
+        <tr>
+          <td>{hypeColors[2]}</td>
+        </tr>
+        <tr>
+          <td>{hypeColors[3]}</td>
+        </tr>
+        <tr>
+          <td>{hypeColors[4]}</td>
+        </tr>
+      </table>
     </div>
-
-    /*
-<div className="Places">
-
-[ Confused as to why this code
-1. makes it to where I cannot use the arrays
-2. breaks everything ]
-
-<table>
-  <th>First Name</th>
-  <tr>
-    <td>{hypePlaces[0]}</td>
-  </tr>
-  <tr>
-    <td>{hypePlaces[1]}</td>
-  </tr>
-  <tr>
-    <td>{hypePlaces[2]}</td>
-  </tr>
-  <tr>
-    <td>{hypePlaces[3]}</td>
-  </tr>
-  <tr>
-    <td>{hypePlaces[4]}</td>
-  </tr>
-</table>
-</div>
-*/
   );
 }
 
 let hypeNames = ["Melody", "Maxwell", "Jean", "Judy", "Reginea"];
 
-let hypePlaces = [];
+let hypePlaces = ["Brooklyn", "New Jersey", "Manhattan", "Brooklyn", "Queens"];
 
 let hypeColors = [];
 
