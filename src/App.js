98a1080ff628d5531185ccd51fe2c3a4f700bb36
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-12-01 17:28:44 -0500

    colors and columns for rows set up, just need more lists thanks to aland and ye for their help, the real MVPs (helped with CSS box outlines and inline styles for colors)

--- src/App.js
+++ src/App.js
@@ -2,59 +2,38 @@ import "./styles.css";
 //Write array below
 export default function App() {
   return (
-    <div className="First Names">
+    <div className="Entire List">
       <table>
-        <th>First Name</th>
+        <tr>
+          <th>First Name</th>
+          <th>Places</th>
+          <th>Colors</th>
+        </tr>
+
         <tr>
           <td>{hypeNames[0]}</td>
+          <td>{hypePlaces[0]}</td>
+          <td style={{ backgroundColor: hypeColors[0] }}>{hypeColors[0]}</td>
         </tr>
         <tr>
           <td>{hypeNames[1]}</td>
+          <td>{hypePlaces[1]}</td>
+          <td style={{ backgroundColor: hypeColors[1] }}>{hypeColors[1]}</td>
         </tr>
         <tr>
           <td>{hypeNames[2]}</td>
+          <td>{hypePlaces[2]}</td>
+          <td style={{ backgroundColor: hypeColors[2] }}>{hypeColors[2]}</td>
         </tr>
         <tr>
           <td>{hypeNames[3]}</td>
+          <td>{hypePlaces[3]}</td>
+          <td style={{ backgroundColor: hypeColors[3] }}>{hypeColors[3]}</td>
         </tr>
         <tr>
           <td>{hypeNames[4]}</td>
-        </tr>
-      </table>
-      <table>
-        <th>Places</th>
-        <tr>
-          <td>{hypePlaces[0]}</td>
-        </tr>
-        <tr>
-          <td>{hypePlaces[1]}</td>
-        </tr>
-        <tr>
-          <td>{hypePlaces[2]}</td>
-        </tr>
-        <tr>
-          <td>{hypePlaces[3]}</td>
-        </tr>
-        <tr>
           <td>{hypePlaces[4]}</td>
-        </tr>
-      </table>
-      <table>
-        <th>Places</th>
-        <tr>
-          <td>{hypeColors[0]}</td>
-        </tr>
-        <tr>
-          <td>{hypeColors[1]}</td>
-        </tr>
-        <tr>
-          <td>{hypeColors[2]}</td>
-        </tr>
-        <tr>
-          <td>{hypeColors[3]}</td>
-        </tr>
-        <tr>
-          <td>{hypeColors[4]}</td>
+          <td style={{ backgroundColor: hypeColors[4] }}>{hypeColors[4]}</td>
         </tr>
       </table>
     </div>
@@ -65,6 +44,11 @@ let hypeNames = ["Melody", "Maxwell", "Jean", "Judy", "Reginea"];
 
 let hypePlaces = ["Brooklyn", "New Jersey", "Manhattan", "Brooklyn", "Queens"];
 
-let hypeColors = [];
+let hypeColors = ["#c8a2c8", "#ebba34", "#2a693b", "#cd94f2", " #eb7d34"];
 
-let hype;
+// placeholder till I display the colors myself
+// at least in text?
+
+let hypeYear = ["2003", "1992", "2005", "1991", "2005"];
+
+let hypeMonth = ["May", "February", "January", "January", "August"];
