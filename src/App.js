c12945993a111c9d54a3330c47802c208420cf20
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-12-01 17:34:33 -0500

    Added months and years to the hypelist this is like my third commit in 10 minutes

--- src/App.js
+++ src/App.js
@@ -8,32 +8,44 @@ export default function App() {
           <th>First Name</th>
           <th>Places</th>
           <th>Colors</th>
+          <th>Month of Birth</th>
+          <th>Year of Birth</th>
         </tr>
 
         <tr>
           <td>{hypeNames[0]}</td>
           <td>{hypePlaces[0]}</td>
           <td style={{ backgroundColor: hypeColors[0] }}>{hypeColors[0]}</td>
+          <td>{hypeMonths[0]}</td>
+          <td>{hypeYears[0]}</td>
         </tr>
         <tr>
           <td>{hypeNames[1]}</td>
           <td>{hypePlaces[1]}</td>
           <td style={{ backgroundColor: hypeColors[1] }}>{hypeColors[1]}</td>
+          <td>{hypeMonths[1]}</td>
+          <td>{hypeYears[1]}</td>
         </tr>
         <tr>
           <td>{hypeNames[2]}</td>
           <td>{hypePlaces[2]}</td>
           <td style={{ backgroundColor: hypeColors[2] }}>{hypeColors[2]}</td>
+          <td>{hypeMonths[2]}</td>
+          <td>{hypeYears[2]}</td>
         </tr>
         <tr>
           <td>{hypeNames[3]}</td>
           <td>{hypePlaces[3]}</td>
           <td style={{ backgroundColor: hypeColors[3] }}>{hypeColors[3]}</td>
+          <td>{hypeMonths[3]}</td>
+          <td>{hypeYears[3]}</td>
         </tr>
         <tr>
           <td>{hypeNames[4]}</td>
           <td>{hypePlaces[4]}</td>
           <td style={{ backgroundColor: hypeColors[4] }}>{hypeColors[4]}</td>
+          <td>{hypeMonths[4]}</td>
+          <td>{hypeYears[4]}</td>
         </tr>
       </table>
     </div>
@@ -46,6 +58,6 @@ let hypePlaces = ["Brooklyn", "New Jersey", "Manhattan", "Brooklyn", "Queens"];
 
 let hypeColors = ["#c8a2c8", "#ebba34", "#2a693b", "#cd94f2", " #eb7d34"];
 
-let hypeYear = ["2003", "1992", "2005", "1991", "2005"];
+let hypeYears = ["2003", "1992", "2005", "1991", "2005"];
 
-let hypeMonth = ["May", "February", "January", "January", "August"];
+let hypeMonths = ["May", "February", "January", "January", "August"];
