e312b8d07ce41867f88bcb982dc201511f27b826
WaluigiTime <43816974+WaluigiTime@users.noreply.github.com>
2021-12-18 15:08:35 -0500

    more polishing of code added more categories that I felt were the most important

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ export default function App() {
           <th>Colors</th>
           <th>Month of Birth</th>
           <th>Year of Birth</th>
+          <th>Dream Place to Visit</th>
+          <th>Favorite Food</th>
+          <th>Prefer Cats or Dogs</th>
+          <th>Hobbies</th>
+          <th>Career You're Interested In</th>
         </tr>
 
         <tr>
@@ -18,6 +23,11 @@ export default function App() {
           <td style={{ backgroundColor: hypeColors[0] }}>{hypeColors[0]}</td>
           <td>{hypeMonths[0]}</td>
           <td>{hypeYears[0]}</td>
+          <td>{hypeDreams[0]}</td>
+          <td>{hypeFoods[0]}</td>
+          <td>{hypePets[0]}</td>
+          <td>{hypeHobbies[0]}</td>
+          <td>{hypeCareers[0]}</td>
         </tr>
         <tr>
           <td>{hypeNames[1]}</td>
@@ -25,6 +35,11 @@ export default function App() {
           <td style={{ backgroundColor: hypeColors[1] }}>{hypeColors[1]}</td>
           <td>{hypeMonths[1]}</td>
           <td>{hypeYears[1]}</td>
+          <td>{hypeDreams[1]}</td>
+          <td>{hypeFoods[1]}</td>
+          <td>{hypePets[1]}</td>
+          <td>{hypeHobbies[1]}</td>
+          <td>{hypeCareers[1]}</td>
         </tr>
         <tr>
           <td>{hypeNames[2]}</td>
@@ -32,6 +47,11 @@ export default function App() {
           <td style={{ backgroundColor: hypeColors[2] }}>{hypeColors[2]}</td>
           <td>{hypeMonths[2]}</td>
           <td>{hypeYears[2]}</td>
+          <td>{hypeDreams[2]}</td>
+          <td>{hypeFoods[2]}</td>
+          <td>{hypePets[2]}</td>
+          <td>{hypeHobbies[2]}</td>
+          <td>{hypeCareers[2]}</td>
         </tr>
         <tr>
           <td>{hypeNames[3]}</td>
@@ -39,6 +59,11 @@ export default function App() {
           <td style={{ backgroundColor: hypeColors[3] }}>{hypeColors[3]}</td>
           <td>{hypeMonths[3]}</td>
           <td>{hypeYears[3]}</td>
+          <td>{hypeDreams[3]}</td>
+          <td>{hypeFoods[3]}</td>
+          <td>{hypePets[3]}</td>
+          <td>{hypeHobbies[3]}</td>
+          <td>{hypeCareers[3]}</td>
         </tr>
         <tr>
           <td>{hypeNames[4]}</td>
@@ -46,6 +71,11 @@ export default function App() {
           <td style={{ backgroundColor: hypeColors[4] }}>{hypeColors[4]}</td>
           <td>{hypeMonths[4]}</td>
           <td>{hypeYears[4]}</td>
+          <td>{hypeDreams[4]}</td>
+          <td>{hypeFoods[4]}</td>
+          <td>{hypePets[4]}</td>
+          <td>{hypeHobbies[4]}</td>
+          <td>{hypeCareers[4]}</td>
         </tr>
       </table>
     </div>
@@ -103,3 +133,37 @@ let hypeColors = ["#c8a2c8", "#ebba34", "#2a693b", "#cd94f2", " #eb7d34"];
 let hypeYears = ["2003", "1992", "2005", "1991", "2005"];
 
 let hypeMonths = ["May", "February", "January", "January", "August"];
+
+let hypeDreams = [
+  "Fukuoka, Japan",
+  "France",
+  "Japan",
+  "Korea",
+  "The whole world but Japan and Korea"
+];
+
+let hypeFoods = [
+  "Everything",
+  "Burgers and Fries",
+  "Lasagna",
+  "Ramen",
+  "Pizza"
+];
+
+let hypePets = ["Both, in favor of Cat", "Dog", "Dog", "Dog", "Dog"];
+
+let hypeHobbies = [
+  "Drawing",
+  "Watching Movies",
+  "Video Games",
+  "Reading",
+  "Writing"
+];
+
+let hypeCareers = [
+  "Tech/Art/Fashion",
+  "Teacher",
+  "Software Engineer",
+  "Software Engineer",
+  "Fashion designer, Youtuber, Write/Create an Anime, Write Music"
+];
